08195086a307fb298f73bd2314352f642c189455
Daniel <danielsikora100@gmail.com>
2024-09-23 11:57:44 +0200

    Dodaj pliki projektów.

---
+++ egzaminInf042023/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace egzaminInf042023
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBoxPosition = new System.Windows.Forms.ComboBox();
+            this.txtEmployeeSurname = new System.Windows.Forms.TextBox();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtPasswordLength = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chkSpecialChars = new System.Windows.Forms.CheckBox();
+            this.chkDigits = new System.Windows.Forms.CheckBox();
+            this.chkUppercase = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxPosition);
+            this.groupBox1.Controls.Add(this.txtEmployeeSurname);
+            this.groupBox1.Controls.Add(this.txtEmployeeName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(103, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(320, 312);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dane pracownika";
+            // 
+            // comboBoxPosition
+            // 
+            this.comboBoxPosition.FormattingEnabled = true;
+            this.comboBoxPosition.Items.AddRange(new object[] {
+            "Kierownik",
+            "Starszy programista",
+            "Młodszy programista",
+            "Tester"});
+            this.comboBoxPosition.Location = new System.Drawing.Point(134, 160);
+            this.comboBoxPosition.Name = "comboBoxPosition";
+            this.comboBoxPosition.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxPosition.TabIndex = 6;
+            // 
+            // txtEmployeeSurname
+            // 
+            this.txtEmployeeSurname.Location = new System.Drawing.Point(134, 104);
+            this.txtEmployeeSurname.Name = "txtEmployeeSurname";
+            this.txtEmployeeSurname.Size = new System.Drawing.Size(100, 22);
+            this.txtEmployeeSurname.TabIndex = 4;
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Location = new System.Drawing.Point(134, 51);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(100, 22);
+            this.txtEmployeeName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Stanowisko";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nazwisko";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Imię";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtPasswordLength);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.chkSpecialChars);
+            this.groupBox2.Controls.Add(this.chkDigits);
+            this.groupBox2.Controls.Add(this.chkUppercase);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(724, 68);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(320, 312);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Generowanie hasła";
+            // 
+            // txtPasswordLength
+            // 
+            this.txtPasswordLength.Location = new System.Drawing.Point(150, 51);
+            this.txtPasswordLength.Name = "txtPasswordLength";
+            this.txtPasswordLength.Size = new System.Drawing.Size(100, 22);
+            this.txtPasswordLength.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SteelBlue;
+            this.button2.Location = new System.Drawing.Point(67, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Generuj hasło";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btnGeneratePassword_Click);
+            // 
+            // chkSpecialChars
+            // 
+            this.chkSpecialChars.AutoSize = true;
+            this.chkSpecialChars.Location = new System.Drawing.Point(34, 185);
+            this.chkSpecialChars.Name = "chkSpecialChars";
+            this.chkSpecialChars.Size = new System.Drawing.Size(124, 20);
+            this.chkSpecialChars.TabIndex = 4;
+            this.chkSpecialChars.Text = "Znaki specjalne";
+            this.chkSpecialChars.UseVisualStyleBackColor = true;
+            // 
+            // chkDigits
+            // 
+            this.chkDigits.AutoSize = true;
+            this.chkDigits.Location = new System.Drawing.Point(34, 141);
+            this.chkDigits.Name = "chkDigits";
+            this.chkDigits.Size = new System.Drawing.Size(59, 20);
+            this.chkDigits.TabIndex = 3;
+            this.chkDigits.Text = "Cyfry";
+            this.chkDigits.UseVisualStyleBackColor = true;
+            // 
+            // chkUppercase
+            // 
+            this.chkUppercase.AutoSize = true;
+            this.chkUppercase.Location = new System.Drawing.Point(34, 100);
+            this.chkUppercase.Name = "chkUppercase";
+            this.chkUppercase.Size = new System.Drawing.Size(143, 20);
+            this.chkUppercase.TabIndex = 2;
+            this.chkUppercase.Text = "Małe i wielkie litery";
+            this.chkUppercase.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Ile znaków?";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.Location = new System.Drawing.Point(493, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Zatwierdź";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1155, 27);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(29, 24);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(1155, 441);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Dodaj pracownika";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkSpecialChars;
+        private System.Windows.Forms.CheckBox chkDigits;
+        private System.Windows.Forms.CheckBox chkUppercase;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmployeeSurname;
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.TextBox txtPasswordLength;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ComboBox comboBoxPosition;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ egzaminInf042023/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace egzaminInf042023
+{
+    // Główna klasa formularza aplikacji Windows Forms
+    public partial class Form1 : Form
+    {
+        // Zmienna do przechowywania wygenerowanego hasła
+        private string generatedPassword = "";
+
+        // Konstruktor klasy Form1, wywołuje metodę InitializeComponent, która tworzy elementy GUI
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Metoda obsługi zdarzenia po kliknięciu przycisku "Generuj hasło"
+        private void btnGeneratePassword_Click(object sender, EventArgs e)
+        {
+            // Pobieranie długości hasła z pola tekstowego
+            int passwordLength = int.Parse(txtPasswordLength.Text);
+
+            // Sprawdzanie, które checkboxy są zaznaczone
+            bool useUppercase = chkUppercase.Checked;
+            bool useDigits = chkDigits.Checked;
+            bool useSpecialChars = chkSpecialChars.Checked;
+
+            // Wywołanie funkcji generującej hasło z odpowiednimi opcjami
+            generatedPassword = GeneratePassword(passwordLength, useUppercase, useDigits, useSpecialChars);
+
+            // Wyświetlenie wygenerowanego hasła w oknie dialogowym
+            MessageBox.Show($"Wygenerowane hasło: {generatedPassword}");
+        }
+
+        // Funkcja generująca hasło na podstawie zadanych parametrów
+        private string GeneratePassword(int length, bool useUppercase, bool useDigits, bool useSpecialChars)
+        {
+            // Zestaw małych liter, który jest domyślnie używany
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+
+            // Zestaw wielkich liter, cyfr i znaków specjalnych
+            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string digits = "0123456789";
+            string specialChars = "!@#$%^&*()_+-=";
+
+            // Początkowy zestaw dozwolonych znaków obejmuje tylko małe litery
+            string allowedChars = lowercase;
+
+            // Lista przechowująca wygenerowane znaki hasła
+            List<char> password = new List<char>();
+
+            // Obiekt Random do losowania znaków
+            Random random = new Random();
+
+            // Jeśli zaznaczono checkbox wielkich liter, dodaj wielkie litery do dozwolonych znaków
+            // i dodaj losową wielką literę do hasła
+            if (useUppercase)
+            {
+                allowedChars += uppercase;
+                password.Add(uppercase[random.Next(uppercase.Length)]);
+            }
+
+            // Jeśli zaznaczono checkbox cyfr, dodaj cyfry do dozwolonych znaków
+            // i dodaj losową cyfrę do hasła
+            if (useDigits)
+            {
+                allowedChars += digits;
+                password.Add(digits[random.Next(digits.Length)]);
+            }
+
+            // Jeśli zaznaczono checkbox znaków specjalnych, dodaj je do dozwolonych znaków
+            // i dodaj losowy znak specjalny do hasła
+            if (useSpecialChars)
+            {
+                allowedChars += specialChars;
+                password.Add(specialChars[random.Next(specialChars.Length)]);
+            }
+
+            // Wypełnianie pozostałych miejsc w haśle losowymi znakami z allowedChars
+            while (password.Count < length)
+            {
+                password.Add(allowedChars[random.Next(allowedChars.Length)]);
+            }
+
+            // Przetasowanie hasła, aby znaki były rozmieszczone losowo
+            return new string(password.OrderBy(x => random.Next()).ToArray());
+        }
+
+        // Metoda obsługi zdarzenia po kliknięciu przycisku "Zatwierdź"
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // Pobieranie danych pracownika z pól tekstowych i combo box
+            string employeeName = txtEmployeeName.Text;
+            string employeeSurname = txtEmployeeSurname.Text;
+            string position = comboBoxPosition.SelectedItem.ToString();
+
+            // Wyświetlanie danych pracownika oraz wygenerowanego hasła w oknie dialogowym
+            MessageBox.Show($"Dane pracownika:{employeeName} {employeeSurname} {position} Hasło:  {generatedPassword}");
+        }
+    }
+}
